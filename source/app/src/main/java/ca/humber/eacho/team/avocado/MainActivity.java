804a8763ba3a48518df9bfff02ef0bd312e578c0
Leonardo Otoni <leonardootoni@gmail.com>
2018-06-24 19:10:19 -0400

    first project push

---
+++ source/app/src/main/java/ca/humber/eacho/team/avocado/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.humber.eacho.team.avocado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
