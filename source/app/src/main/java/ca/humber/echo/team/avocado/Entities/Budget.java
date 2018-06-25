c187796a834cbb409c84b20a15c09fb7b7ac1d8a
Leonardo Otoni <leonardootoni@gmail.com>
2018-06-24 20:54:27 -0400

    Entity Java Bean Classes for the Avocado app

---
+++ source/app/src/main/java/ca/humber/echo/team/avocado/Entities/Budget.java
@@ -0,0 +1,42 @@
+package ca.humber.echo.team.avocado.Entities;
+
+
+/**
+ * Entity Java Bean class
+ */
+public class Budget {
+
+    private Long id;
+    private String name;
+    private Long referenceId;
+
+    public Budget(Long id, String name, Long referenceId) {
+        this.id = id;
+        this.name = name;
+        this.referenceId = referenceId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getReferenceId() {
+        return referenceId;
+    }
+
+    public void setReferenceId(Long referenceId) {
+        this.referenceId = referenceId;
+    }
+}
