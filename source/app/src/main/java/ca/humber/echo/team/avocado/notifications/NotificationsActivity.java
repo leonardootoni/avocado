f2ff91b75a2605c735d91b160b739c1c3f43ad8c
Leonardo Otoni <leonardootoni@gmail.com>
2018-06-24 20:15:21 -0400

    Use Cases 6 and 7 created. Just empty activities. strings.xml divided into Use Cases sections (proposal).

---
+++ source/app/src/main/java/ca/humber/echo/team/avocado/notifications/NotificationsActivity.java
@@ -0,0 +1,15 @@
+package ca.humber.echo.team.avocado.notifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.humber.echo.team.avocado.R;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
+}
