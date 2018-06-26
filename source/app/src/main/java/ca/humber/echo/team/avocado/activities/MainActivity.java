23873fa0a4f835acdd9aba5e182fb623228e17ed
Leonardo Otoni <leonardootoni@gmail.com>
2018-06-25 22:39:43 -0400

    Exemple to get a database instance and recover data.

--- source/app/src/main/java/ca/humber/echo/team/avocado/activities/MainActivity.java
+++ source/app/src/main/java/ca/humber/echo/team/avocado/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package ca.humber.echo.team.avocado.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import ca.humber.echo.team.avocado.R;
+import ca.humber.echo.team.avocado.repository.database.AvocadoDataBase;
+import ca.humber.echo.team.avocado.repository.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String message = "";
+        try{
+            User user = AvocadoDataBase.getInstance(this).userDAO().getUserByEmail("leo@leo.com");
+            message = user.getLastName();
+        }catch(Exception ex){
+            message = ex.getMessage();
+        }
+
+        int a = 0;
+
     }
+
 }
